abf7e56f2c7bc5db9f7582d4676f7428dc645044
Jvjesus89 <89112456+Jvjesus89@users.noreply.github.com>
2023-03-25 15:36:36 -0300

    Incluindo sistema

---
+++ bytebank/Titular/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace bytebank.Titular
+{
+    public class Cliente
+    {
+        private string nome;
+        public string Nome { 
+         get{ return nome ;}
+         set 
+            { 
+                Regex somenteletra = new Regex(@"[a-zA-Z]+.[a-zA-Z]+");
+                bool resposta = somenteletra.IsMatch(value);
+                if (resposta == true)
+                {
+                    this.nome = value;
+                }
+                else
+                {
+                    Console.WriteLine("Digite apenas letras");
+                    return;
+                }
+            }
+        }
+        private string cpf;
+            public string Cpf
+            {
+                get { return this.cpf; }
+                set { this.cpf = value; }
+            }
+
+        public string Profissao { get; set; }
+    }
+}
